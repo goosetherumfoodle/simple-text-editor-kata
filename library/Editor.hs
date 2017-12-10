ff2089522c941b5ea49a3f930d4ff035d9935a6f
ugly_bear <goosetherumfoodle@users.noreply.github.com>
2017-12-09 20:30:27 -0500

    finishes appending, deleting, and printing

--- library/Editor.hs
+++ library/Editor.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Editor (main
                , perform
                , Command(..)
@@ -6,12 +8,21 @@ module Editor (main
                , OutputString(..)
                , initialState) where
 
--- TODO: protect undoable command list (in state) from getting a non-undoable command
 
-type OutputString =  String
-type InternalString = String
+import Data.Text.Lazy (Text(..)
+                      , dropEnd
+                      , append
+                      , index)
+import Data.Int (Int64)
 
-data Command = Delete Int | Append String | Print Int | Undo deriving (Show, Eq)
+-- TODO: 1: protect undoable command list (in state) from getting a non-undoable command
+-- 2: text string type
+-- 3: protect integers from being unnatural numbers
+
+type OutputString = String
+type InternalString = Text
+
+data Command = Delete Int64 | Append Text | Print Int64 | Undo deriving (Show, Eq)
 data State = State {
     getInternal :: InternalString
   , getOutput :: OutputString
@@ -21,7 +32,11 @@ data State = State {
 initialState = State "" "" []
 
 perform :: State -> Command -> State
-perform s cmd@(Append string) = State ((getInternal s) ++ string) ""  (cmd : (getHistory s))
+perform s cmd@(Append str) = State (append (getInternal s) str) (getOutput s) (cmd : (getHistory s))
+perform s cmd@(Delete i) = State (dropEnd i (getInternal s)) (getOutput s) (cmd : (getHistory s))
+perform s cmd@(Print i) = State (getInternal s) ((index (getInternal s) (baseZero i)) : (getOutput s)) (getHistory s)
+  where
+    baseZero i = i - 1
 
 
 main :: IO ()
--- test-suite/Main.hs
+++ test-suite/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import qualified Test.Tasty
 import Test.Tasty.Hspec
 import Editor (perform
@@ -18,8 +20,21 @@ spec = parallel $ do
         it "returns correct state" $ do
           let outputState = (State "horse" "" [Append "horse"])
           perform initialState (Append "horse") `shouldBe` outputState
+
       context "to existing state" $ do
         let previousState = (State "initial" "" [Append "initial"])
         it "returns correct state" $ do
           let outputState = (State "initial new" "" [Append " new", Append "initial"])
           perform previousState (Append " new") `shouldBe` outputState
+
+    describe "deleting" $ do
+      it "removes correct number of characters" $ do
+        let previousState = (State "initial state" "" [Append "initial state"])
+            outputState = (State "initial" "" [Delete 6, Append "initial state"])
+        perform previousState (Delete 6) `shouldBe` outputState
+
+    describe "printing" $ do
+      it "conses the correct character into the output" $ do
+        let previousState = (State "initial" "i" [Append "initial"])
+            outputState = (State "initial" "ni" [Append "initial"])
+        perform previousState (Print 2) `shouldBe` outputState
